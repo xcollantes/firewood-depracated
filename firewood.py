191bedc8d1106e792e766e70ab53a99c5f133397
Xavier Collantes <collantes.xavier@gmail.com>
2020-04-24 01:16:39 -0700

    Add logging

--- firewood.py
+++ firewood.py
@@ -2,13 +2,20 @@
 # firewood.py
 
 import logging
+import os
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+logging.basicConfig(
+  format='%(levelname)s %(asctime)s:%(message)s',
+  datefmt='%Y-%m-%d %H:%M:%S %Z',
+  level=logging.DEBUG
+)
+
+
 URL = 'https://tinder.com/'
 WEBDRIVER_PATH = '/Users/xcollantes/Documents/firewood/chromedriver81'
-
 
 def main():
   driver = webdriver.Chrome(WEBDRIVER_PATH)
